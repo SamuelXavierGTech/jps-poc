7e20b63791729754958d641daea0f85b35f9a606
xavier <samuelpedroso08@gmail.com>
2024-09-27 09:49:11 -0300

    commit

---
+++ src/test/java/com/example/jbspoc/JbsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jbspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
